64e2b533c2b41b367f5594a7a7f5f00f108f0657
Francesco Noacco <francesco.noacco2000@gmail.com>
2021-12-01 08:43:51 +0100

    add check for nix file source

--- home/modules/not-nixos.nix
+++ home/modules/not-nixos.nix
@@ -13,6 +13,10 @@
       }
     ];
     
-    shellInit = "fenv source ~/.nix-profile/etc/profile.d/nix.sh";
+    shellInit = ''
+      if test -f ~/.nix-profile/etc/profile.d/nix.sh
+        fenv source ~/.nix-profile/etc/profile.d/nix.sh
+      end
+    '';
   };
 }
