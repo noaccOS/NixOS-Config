9bc5d5514634440587f9eef7fe2be72acb6d1be5
Francesco Noacco <francesco.noacco2000@gmail.com>
2023-02-02 17:23:29 +0100

    remove old packages

--- packages/default.nix
+++ packages/default.nix
@@ -1,9 +1,3 @@
 self: super: rec {
-  comma           = super.callPackage ./comma           {};
-  # emacsIDE           = super.callPackage ./emacsIDE           {};
-  # wine-ge            = super.callPackage ./wine-ge            {};
-  wezterm-nightly = super.callPackage ./wezterm-nightly {};
-  xcursor-breeze  = super.callPackage ./xcursor-breeze  {};
-  spotify-adblock = super.callPackage ./spotify-adblock {};
-  emacs-ng        = super.callPackage ./emacs-ng        {};
+  # comma           = super.callPackage ./comma           {};
 }
