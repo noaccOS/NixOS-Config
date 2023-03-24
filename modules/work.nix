02dd844a99785c827c4bbad04289d388786aa57d
Francesco Noacco <francesco.noacco@secomind.com>
2023-03-24 11:05:54 +0100

    Install mattermost at work

--- modules/work.nix
+++ modules/work.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }:
 {
   imports = [ ./desktop.nix ];
+
+  environment.defaultPackages = with pkgs; [
+    mattermost-desktop
+  ];
+
   users.users.tech = {
     hashedPassword = "$y$j9T$EYMiCulten0c9xEkSW59l0$IYR43m.6UjbNP9GbHI05uG.qbNYLPI5cA00s97xnWS5";
     uid = 1000;
