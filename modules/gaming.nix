a0e85effabc33758d13c26ac7584f64978ef86cb
Francesco Noacco <francesco.noacco2000@gmail.com>
2023-07-10 19:31:27 +0200

    fix(gaming): downgrade ibus to fix sticky keys
    Signed-off-by: Francesco Noacco <francesco.noacco2000@gmail.com>

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,4 +1,4 @@
-{ lib, config, pkgs, ... }:
+{ lib, config, pkgs, currentSystem, ... }:
 let
   kp = config.boot.kernelPackages;
   cfg = config.noaccOSModules.gaming;
@@ -11,6 +11,22 @@ in
   config = lib.mkIf cfg.enable {
     boot.extraModulePackages = [ kp.xpadneo ];
 
+    nixpkgs.overlays =
+      let
+        pkgs_ibus_27 = import
+          (pkgs.fetchFromGitHub {
+            owner = "NixOS";
+            repo = "nixpkgs";
+            rev = "8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8";
+            sha256 = "sha256-0gI2FHID8XYD3504kLFRLH3C2GmMCzVMC50APV/kZp8=";
+          })
+          { system = currentSystem; };
+      in
+      [
+        (final: prev: {
+          ibus = pkgs_ibus_27.ibus;
+        })
+      ];
 
     environment.systemPackages = with pkgs; [
       wineWowPackages.staging
