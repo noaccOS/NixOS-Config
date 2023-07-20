7be473809f548643304a81ba9b1bd3e444bf8e3f
Francesco Noacco <francesco.noacco2000@gmail.com>
2023-07-20 16:00:16 +0200

    feat(kaiki): add swapfile
    Signed-off-by: Francesco Noacco <francesco.noacco2000@gmail.com>

--- hosts/kaiki.nix
+++ hosts/kaiki.nix
@@ -22,7 +22,7 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [{ device = "/var/lib/swapfile"; size = 104857600; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
