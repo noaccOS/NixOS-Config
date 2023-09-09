0d24980a41632de943ee34baaec5b068e744f940
Francesco Noacco <francesco.noacco2000@gmail.com>
2023-09-09 11:58:48 +0200

    style(cli): nix fmt
    Signed-off-by: Francesco Noacco <francesco.noacco2000@gmail.com>

--- home/modules/cli.nix
+++ home/modules/cli.nix
@@ -65,135 +65,135 @@ in
         '';
 
         shellInit = lib.strings.optionalString cfg.sourceNix ''
-
-    if test -f ~/.nix-profile/etc/profile.d/nix.fish
-      source ~/.nix-profile/etc/profile.d/nix.fish
-    end
-  '';
+          if test -f ~/.nix-profile/etc/profile.d/nix.fish
+            source ~/.nix-profile/etc/profile.d/nix.fish
+          end
+        '';
       };
 
-    git = {
-      enable = true;
-      difftastic.enable = true;
-      ignores = [
-        "log/"
-        ".direnv/"
-        ".nix-mix/"
-        ".nix-hex/"
-        ".envrc"
-      ];
-      extraConfig = {
-        init.defaultBranch = "main";
-        pull.ff = "only";
-        github.user = "noaccOS";
-        diff.algorithm = "patience";
-        credential.helper = "store";
+      git = {
+        enable = true;
+        difftastic.enable = true;
+        ignores = [
+          "log/"
+          ".direnv/"
+          ".nix-mix/"
+          ".nix-hex/"
+          ".envrc"
+        ];
+        extraConfig = {
+          init.defaultBranch = "main";
+          pull.ff = "only";
+          github.user = "noaccOS";
+          diff.algorithm = "patience";
+          credential.helper = "store";
 
-        user = {
-          name = "Francesco Noacco";
-          email = "francesco.noacco2000@gmail.com";
-        };
-      };
-
-      includes = [{
-        condition = "gitdir:~/src/seco/";
-        contents = {
           user = {
-            email = "francesco.noacco@secomind.com";
-            signingKey = "A83DA1B14BD444A6";
+            name = "Francesco Noacco";
+            email = "francesco.noacco2000@gmail.com";
           };
-          commit.gpgSign = true;
         };
-      }];
 
-    };
+        includes = [{
+          condition = "gitdir:~/src/seco/";
+          contents = {
+            user = {
+              email = "francesco.noacco@secomind.com";
+              signingKey = "A83DA1B14BD444A6";
+            };
+            commit.gpgSign = true;
+          };
+        }];
 
-    neovim = {
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-    };
+      };
 
-    starship = {
-      enable = true;
-      enableFishIntegration = true;
-      settings = {
-        aws.symbol = "  ";
-        buf.symbol = " ";
-        c.symbol = " ";
-        conda.symbol = " ";
-        dart.symbol = " ";
-        directory.read_only = " 󰌾";
-        docker_context.symbol = " ";
-        elixir.symbol = " ";
-        elm.symbol = " ";
-        fossil_branch.symbol = " ";
-        git_branch.symbol = " ";
-        golang.symbol = " ";
-        guix_shell.symbol = " ";
-        haskell.symbol = " ";
-        haxe.symbol = " ";
-        hg_branch.symbol = " ";
-        hostname.ssh_symbol = " ";
-        java.symbol = " ";
-        julia.symbol = " ";
-        lua.symbol = " ";
-        memory_usage.symbol = "󰍛 ";
-        meson.symbol = "󰔷 ";
-        nim.symbol = "󰆥 ";
-        nix_shell = { symbol = " "; heuristic = true; };
-        nodejs.symbol = " ";
-        package.symbol = "󰏗 ";
-        pijul_channel.symbol = " ";
-        python.symbol = " ";
-        rlang.symbol = "󰟔 ";
-        ruby.symbol = " ";
-        rust.symbol = " ";
-        scala.symbol = " ";
+      neovim = {
+        enable = true;
+        viAlias = true;
+        vimAlias = true;
+      };
 
-        os.symbols = {
-          Alpaquita = " ";
-          Alpine = " ";
-          Amazon = " ";
-          Android = " ";
-          Arch = " ";
-          Artix = " ";
-          CentOS = " ";
-          Debian = " ";
-          DragonFly = " ";
-          Emscripten = " ";
-          EndeavourOS = " ";
-          Fedora = " ";
-          FreeBSD = " ";
-          Garuda = "󰛓 ";
-          Gentoo = " ";
-          HardenedBSD = "󰞌 ";
-          Illumos = "󰈸 ";
-          Linux = " ";
-          Mabox = " ";
-          Macos = " ";
-          Manjaro = " ";
-          Mariner = " ";
-          MidnightBSD = " ";
-          Mint = " ";
-          NetBSD = " ";
-          NixOS = " ";
-          OpenBSD = "󰈺 ";
-          openSUSE = " ";
-          OracleLinux = "󰌷 ";
-          Pop = " ";
-          Raspbian = " ";
-          Redhat = " ";
-          RedHatEnterprise = " ";
-          Redox = "󰀘 ";
-          Solus = "󰠳 ";
-          SUSE = " ";
-          Ubuntu = " ";
-          Unknown = " ";
-          Windows = "󰍲 ";
+      starship = {
+        enable = true;
+        enableFishIntegration = true;
+        settings = {
+          aws.symbol = "  ";
+          buf.symbol = " ";
+          c.symbol = " ";
+          conda.symbol = " ";
+          dart.symbol = " ";
+          directory.read_only = " 󰌾";
+          docker_context.symbol = " ";
+          elixir.symbol = " ";
+          elm.symbol = " ";
+          fossil_branch.symbol = " ";
+          git_branch.symbol = " ";
+          golang.symbol = " ";
+          guix_shell.symbol = " ";
+          haskell.symbol = " ";
+          haxe.symbol = " ";
+          hg_branch.symbol = " ";
+          hostname.ssh_symbol = " ";
+          java.symbol = " ";
+          julia.symbol = " ";
+          lua.symbol = " ";
+          memory_usage.symbol = "󰍛 ";
+          meson.symbol = "󰔷 ";
+          nim.symbol = "󰆥 ";
+          nix_shell = { symbol = " "; heuristic = true; };
+          nodejs.symbol = " ";
+          package.symbol = "󰏗 ";
+          pijul_channel.symbol = " ";
+          python.symbol = " ";
+          rlang.symbol = "󰟔 ";
+          ruby.symbol = " ";
+          rust.symbol = " ";
+          scala.symbol = " ";
+
+          os.symbols = {
+            Alpaquita = " ";
+            Alpine = " ";
+            Amazon = " ";
+            Android = " ";
+            Arch = " ";
+            Artix = " ";
+            CentOS = " ";
+            Debian = " ";
+            DragonFly = " ";
+            Emscripten = " ";
+            EndeavourOS = " ";
+            Fedora = " ";
+            FreeBSD = " ";
+            Garuda = "󰛓 ";
+            Gentoo = " ";
+            HardenedBSD = "󰞌 ";
+            Illumos = "󰈸 ";
+            Linux = " ";
+            Mabox = " ";
+            Macos = " ";
+            Manjaro = " ";
+            Mariner = " ";
+            MidnightBSD = " ";
+            Mint = " ";
+            NetBSD = " ";
+            NixOS = " ";
+            OpenBSD = "󰈺 ";
+            openSUSE = " ";
+            OracleLinux = "󰌷 ";
+            Pop = " ";
+            Raspbian = " ";
+            Redhat = " ";
+            RedHatEnterprise = " ";
+            Redox = "󰀘 ";
+            Solus = "󰠳 ";
+            SUSE = " ";
+            Ubuntu = " ";
+            Unknown = " ";
+            Windows = "󰍲 ";
+          };
         };
       };
-    };
 
+    };
   };
 }
