b42ec349740e96e3de7bf06a9c91db2d8d2754d2
Francesco Noacco <francesco.noacco2000@gmail.com>
2023-10-16 09:22:46 +0200

    refactor(dev): one option for each language
    Signed-off-by: Francesco Noacco <francesco.noacco2000@gmail.com>

--- modules/development.nix
+++ modules/development.nix
@@ -9,8 +9,19 @@ let
   cfg = config.noaccOSModules.development;
 in
 {
-  options.noaccOSModules.development = {
-    enable = lib.mkEnableOption "Dev tools, mostly languages and compilers";
+  options.noaccOSModules.development = with lib; {
+    enable = mkEnableOption "Dev tools, mostly languages and compilers";
+    c.enable = mkEnableOption "C";
+    cSharp.enable = mkEnableOption "C#";
+    elixir.enable = mkEnableOption "Elixir" // { default = true; };
+    haskell.enable = mkEnableOption "Haskell" // { default = true; };
+    java.enable = mkEnableOption "Java";
+    latex.enable = mkEnableOption "LaTeX";
+    lua.enable = mkEnableOption "Lua";
+    nix.enable = mkEnableOption "Nix" // { default = true; };
+    rust.enable = mkEnableOption "Rust" // { default = true; };
+    python.enable = mkEnableOption "Python" // { default = true; };
+    racket.enable = mkEnableOption "Racket";
   };
 
   config = lib.mkIf cfg.enable {
@@ -20,64 +31,59 @@ in
 
     environment.systemPackages = with pkgs; [
       (tree-sitter.withPlugins (_: tree-sitter.allGrammars))
-
-      # Nix
-      nixpkgs-fmt
-      nil
-
-      # Haskell
-      ghc # Compiler
-      stack # CLI tool
-      haskell-language-server # LSP
-      haskellPackages.hindent # Indent
-
-      # Python
-      myPython # Compiler
-      nodePackages.pyright # LSP
-
-      # C/C++
+      (aspellWithDicts (ps: with ps; [ en it ]))
+    ]
+    ++ lib.optionals cfg.c.enable [
       clang # Compiler
       gcc # Compiler
       ccls # LSP
-
-      # C#
+    ]
+    ++ lib.optionals cfg.cSharp.enable [
       dotnet-sdk # Compiler
       dotnet-runtime # Runner
       mono # Runner
       omnisharp-roslyn # LSP
-
-      # LaTeX
-      texlab # LSP
-      python3Packages.pygments # Minted support
-      texlive.combined.scheme-small # Compiler and libraries
-
-      # Lua
-      lua # Compiler
-
-      # Rust
-      rustc
-      cargo
-      rust-analyzer # LSP
-      rustfmt # Formatter
-
-      # Racket
-      racket # Compiler
-
-      # Java
-      jdk # Compiler
-      jdt-language-server # LSP
-
-      # Elixir
+    ]
+    ++ lib.optionals cfg.elixir.enable [
       elixir # Compiler
       elixir_ls # LSP
       erlang # Needed for escript
-
-      (aspellWithDicts (ps: with ps; [ en it ]))
+    ]
+    ++ lib.optionals cfg.haskell.enable [
+      ghc # Compiler
+      stack # CLI tool
+      haskell-language-server # LSP
+      haskellPackages.hindent # Indent
+    ]
+    ++ lib.optionals cfg.java.enable [
+      jdk # Compiler
+      jdt-language-server # LSP
+    ]
+    ++ lib.optionals cfg.latex.enable [
+      texlab # LSP
+      python3Packages.pygments # Minted support
+      texlive.combined.scheme-small # Compiler and libraries
+    ]
+    ++ lib.optionals cfg.lua.enable [
+      lua # Compiler
+    ]
+    ++ lib.optionals cfg.nix.enable [
+      nixpkgs-fmt # Formatter
+      nil # LSP
+    ]
+    ++ lib.optionals cfg.rust.enable [
+      rustc # Compiler
+      cargo # Project Manager
+      rust-analyzer # LSP
+      rustfmt # Formatter
+    ]
+    ++ lib.optionals cfg.python.enable [
+      myPython # Compiler
+      nodePackages.pyright # LSP
+    ]
+    ++ lib.optionals cfg.racket.enable [
+      racket # Compiler
     ];
-
-    services = {
-      postgresql.enable = true;
-    };
   };
 
 }
