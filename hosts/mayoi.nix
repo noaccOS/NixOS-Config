1037f9f8ca592f101211b461cc961f77e8b0e5d5
Francesco Noacco <francesco.noacco2000@gmail.com>
2023-12-18 11:28:58 +0100

    feat!(mayoi): remove custom X config
    Signed-off-by: Francesco Noacco <francesco.noacco2000@gmail.com>

--- hosts/mayoi.nix
+++ hosts/mayoi.nix
@@ -34,23 +34,4 @@
     };
 
   swapDevices = [ ];
-
-  services.xserver = {
-    exportConfiguration = true;
-    monitorSection = ''
-      VendorName     "Unknown"
-      ModelName      "HPN OMEN by HP 35"
-      HorizSync       73.0 - 151.0
-      VertRefresh     30.0 - 100.0
-      Option         "DPMS"
-    '';
-    screenSection = ''
-      DefaultDepth    24
-      Option         "Stereo" "0"
-      Option         "metamodes" "DP-0: 3440x1440_100 +1920+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, HDMI-0: 1920x1080_60 +0+360 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
-      Option         "SLI" "Off"
-      Option         "MultiGPU" "Off"
-      Option         "BaseMosaic" "off"
-    '';
-  };
 }
