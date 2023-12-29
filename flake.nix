4bf8d637fad8ae8020507dfdf6bf79c11d75b040
Francesco Noacco <francesco.noacco2000@gmail.com>
2023-12-29 19:02:13 +0100

    chore(flake): add rycee nur input
    Flake lock file updates:
    
    • Added input 'rycee':
        'gitlab:rycee/nur-expressions/562f210ae8fd096ccc9d946bbfcc848cf6034689' (2023-12-29)

--- flake.nix
+++ flake.nix
@@ -14,6 +14,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    rycee = {
+      url = "gitlab:rycee/nur-expressions";
+      flake = false;
+    };
 
     # Overrides
     mutter-triple-buffer = {
