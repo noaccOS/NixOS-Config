2830a5cf70b2c117b793d033879a0f30365f86fd
Francesco Noacco <francesco.noacco2000@gmail.com>
2024-03-29 12:59:34 +0100

    feat: remove ibus workaround
    ibus was updated and I'm now using mozc
    
    Signed-off-by: Francesco Noacco <francesco.noacco2000@gmail.com>

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -11,23 +11,6 @@ in
   config = lib.mkIf cfg.enable {
     boot.extraModulePackages = [ kp.xpadneo ];
 
-    nixpkgs.overlays =
-      let
-        pkgs_ibus_27 = import
-          (pkgs.fetchFromGitHub {
-            owner = "NixOS";
-            repo = "nixpkgs";
-            rev = "8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8";
-            sha256 = "sha256-0gI2FHID8XYD3504kLFRLH3C2GmMCzVMC50APV/kZp8=";
-          })
-          { system = currentSystem; };
-      in
-      [
-        (final: prev: {
-          ibus = pkgs_ibus_27.ibus;
-        })
-      ];
-
     environment.systemPackages = with pkgs; [
       wineWowPackages.staging
       winetricks
