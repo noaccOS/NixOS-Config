ab8de77f4c23cdcbd9911ae77661626745707647
Francesco Noacco <francesco.noacco2000@gmail.com>
2024-10-27 18:45:22 +0100

    feat(kitty): define unicode map for japanese

--- home/modules/programs/terminals/kitty.nix
+++ home/modules/programs/terminals/kitty.nix
@@ -1,7 +1,72 @@
 { config, lib, ... }:
 let
-  inherit (lib) mkEnableOption mkIf;
   cfg = config.homeModules.programs.terminals.kitty;
+
+  inherit (lib)
+    attrValues
+    concatStringsSep
+    flatten
+    mkEnableOption
+    mkIf
+    pipe
+    ;
+
+  japanese_code_points = {
+    hiragana = {
+      from = "3041";
+      to = "3096";
+    };
+    katakana = {
+      from = "30A0";
+      to = "30FF";
+    };
+    kanji = [
+      {
+        from = "3400";
+        to = "4DB5";
+      }
+      {
+        from = "4E00";
+        to = "9FCB";
+      }
+      {
+        from = "F900";
+        to = "FA6A";
+      }
+    ];
+    radicals = {
+      from = "2E80";
+      to = "2FD5";
+    };
+    half_width = {
+      from = "FF5F";
+      to = "FF9F";
+    };
+    symbols = {
+      from = "3000";
+      to = "303F";
+    };
+    misc = [
+      {
+        from = "31F0";
+        to = "31FF";
+      }
+      {
+        from = "3220";
+        to = "3243";
+      }
+      {
+        from = "3280";
+        to = "337F";
+      }
+    ];
+    western = {
+      from = "FF01";
+      to = "FF5E";
+    };
+  };
+
+  unicodeRangeToKittyRange = range: "U+${range.from}-U+${range.to}";
 in
 {
   options.homeModules.programs.terminals.kitty = {
@@ -14,12 +79,22 @@ in
       name = "JetBrainsMono Nerd Font";
       size = 13;
     };
-    settings = {
-      cursor_shape = "beam";
-      cursor_blink_interval = "0";
-      mouse_hide_wait = "0";
-      confirm_os_window_close = "0";
-      disable_ligatures_in = "active cursor";
-    };
+    settings =
+      let
+        japanese_map = pipe japanese_code_points [
+          attrValues
+          flatten
+          (map unicodeRangeToKittyRange)
+          (concatStringsSep ",")
+        ];
+      in
+      {
+        cursor_shape = "beam";
+        cursor_blink_interval = "0";
+        mouse_hide_wait = "0";
+        confirm_os_window_close = "0";
+        disable_ligatures_in = "active cursor";
+        symbol_map = "${japanese_map} Noto Sans Mono CJK JP";
+      };
   };
 }
