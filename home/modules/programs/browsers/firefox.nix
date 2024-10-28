d5e69a274f97f1ed8cb3712a2997aff54a2db3c2
Francesco Noacco <francesco.noacco2000@gmail.com>
2024-10-28 09:43:47 +0100

    chore(firefox): add jisho favicon

--- home/modules/programs/browsers/firefox.nix
+++ home/modules/programs/browsers/firefox.nix
@@ -95,6 +95,8 @@ let
 
         Jisho = {
           urls = [ { template = "https://jisho.org/search/{searchTerms}"; } ];
+          iconUpdateURL = "https://assets.jisho.org/assets/favicon-062c4a0240e1e6d72c38aa524742c2d558ee6234497d91dd6b75a182ea823d65.ico";
+          updateInterval = updateDaily;
           definedAliases = [ "@j" ];
         };
 
