a5f6c01a4090d155848eb5f718fa18549514c56b
Francesco Noacco <francesco.noacco2000@gmail.com>
2024-12-10 09:49:32 +0100

    feat(niri): lockscreen and playerctl

--- home/modules/windowManagers/niri.nix
+++ home/modules/windowManagers/niri.nix
@@ -91,6 +91,44 @@ in
       pkgs.xdg-desktop-portal-gnome
     ];
 
+    programs.hyprlock = {
+      enable = true;
+      settings = {
+        general = {
+          disable_loading_bar = true;
+          grace = 5;
+          hide_cursor = true;
+          no_fade_in = false;
+        };
+
+        background = [
+          {
+            path = "screenshot";
+            blur_passes = 3;
+            blur_size = 5;
+          }
+        ];
+
+        input-field = [
+          {
+            size = "200, 50";
+            position = "0, -80";
+            monitor = "";
+            dots_center = true;
+            fade_on_empty = false;
+            font_color = "rgb(202, 211, 245)";
+            inner_color = "rgb(91, 96, 120)";
+            outer_color = "rgb(24, 25, 38)";
+            outline_thickness = 5;
+            placeholder_text = "<i>Input Password...</i>";
+            shadow_passes = 2;
+          }
+        ];
+      };
+    };
+
+    home.packages = [ pkgs.playerctl ];
+
     programs.niri = {
       enable = true;
       package = inputs.niri.packages.${pkgs.system}.niri-unstable;
