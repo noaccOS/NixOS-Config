44710cdbe717cf26e7867817d3e920fdc7bf92ba
Francesco Noacco <francesco.noacco2000@gmail.com>
2025-01-15 12:34:48 +0100

    refactor(ghostty): use home-manager module

--- home/modules/programs/terminals/ghostty.nix
+++ home/modules/programs/terminals/ghostty.nix
@@ -8,49 +8,49 @@
 let
   cfg = config.homeModules.programs.terminals.alacritty;
 
-  inherit (lib) mkEnableOption mkIf;
-
-  ghostty-config = ''
-    theme = catppuccin-mocha
-    window-theme = ghostty
-    font-size = 14
-    clipboard-paste-protection = false
-    mouse-scroll-multiplier = 2.5
-    shell-integration-features = no-cursor
-    cursor-style = bar
-    cursor-style-blink = false
-    unfocused-split-opacity = 0.85
-
-    keybind = clear
-    keybind = alt+y=copy_to_clipboard
-    keybind = alt+p=paste_from_clipboard
-    keybind = alt+r=new_split:right
-    keybind = alt+d=new_split:down
-    keybind = alt+h=goto_split:left
-    keybind = alt+t=goto_split:bottom
-    keybind = alt+n=goto_split:top
-    keybind = alt+s=goto_split:right
-    keybind = alt+m=resize_split:left,100
-    keybind = alt+w=resize_split:down,100
-    keybind = alt+v=resize_split:up,100
-    keybind = alt+z=resize_split:right,100
-    keybind = alt+f=toggle_split_zoom
-    keybind = alt+e=equalize_splits
-    keybind = alt+s=write_scrollback_file:open
-    keybind = alt+u>n=new_window
-    keybind = alt+u>t=new_tab
-    keybind = alt+u>q=close_surface
-    keybind = alt+u>r=reload_config
-  '';
+  inherit (lib) mkEnableOption;
 in
 {
   options.homeModules.programs.terminals.ghostty = {
     enable = mkEnableOption "ghostty";
   };
 
-  config = mkIf (cfg.enable) {
-    home.packages = [ inputs.ghostty.packages.${pkgs.system}.default ];
+  config.programs.ghostty = {
+    enable = cfg.enable;
+    package = inputs.ghostty.packages.${pkgs.system}.default;
+    clearDefaultKeybinds = true;
+    settings = {
+      theme = "catppuccin-mocha";
+      window-theme = "ghostty";
+      font-size = 14;
+      clipboard-paste-protection = false;
+      mouse-scroll-multiplier = 2.5;
+      shell-integration-features = "no-cursor";
+      cursor-style = "bar";
+      cursor-style-blink = false;
+      unfocused-split-opacity = 0.85;
 
-    xdg.configFile."ghostty/config".text = ghostty-config;
+      keybind = [
+        "alt+y=copy_to_clipboard"
+        "alt+p=paste_from_clipboard"
+        "alt+r=new_split:right"
+        "alt+d=new_split:down"
+        "alt+h=goto_split:left"
+        "alt+t=goto_split:bottom"
+        "alt+n=goto_split:top"
+        "alt+s=goto_split:right"
+        "alt+m=resize_split:left,100"
+        "alt+w=resize_split:down,100"
+        "alt+v=resize_split:up,100"
+        "alt+z=resize_split:right,100"
+        "alt+f=toggle_split_zoom"
+        "alt+e=equalize_splits"
+        "alt+s=write_scrollback_file:open"
+        "alt+u>n=new_window"
+        "alt+u>t=new_tab"
+        "alt+u>q=close_surface"
+        "alt+u>r=reload_config"
+      ];
+    };
   };
 }
