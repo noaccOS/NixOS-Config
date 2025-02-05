e782b20044ec5f24196098273d8fd3f7fa2f9899
Francesco Noacco <francesco.noacco2000@gmail.com>
2025-02-05 12:03:41 +0100

    feat(erl): make C-c interrupt input

--- home/modules/development.nix
+++ home/modules/development.nix
@@ -110,7 +110,7 @@ in
     }
     (mkIf cfg.enableTools {
       home.sessionVariables = {
-        ERL_AFLAGS = "-kernel shell_history enabled";
+        ERL_AFLAGS = "-kernel shell_history enabled +B c";
       };
 
       programs.gh.enable = true;
