db8d3df3eef26224dc075c92f13d3e7232fd40d5
Francesco Noacco <francesco.noacco2000@gmail.com>
2025-03-10 11:29:18 +0100

    chore(cachix): fix substituters
    anyrun was not defining its own substituter, probably was a copy-paste
    error

--- modules/cachix/anyrun.nix
+++ modules/cachix/anyrun.nix
@@ -1,6 +1,6 @@
 {
   nix.settings = {
-    extra-substituters = [ "https://helix.cachix.org" ];
+    extra-substituters = [ "https://anyrun.cachix.org" ];
     extra-trusted-public-keys = [
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
     ];
