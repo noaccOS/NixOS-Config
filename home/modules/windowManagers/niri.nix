71b74c48b8a51d2b82a38b3f99e0afdbe136a3fd
Francesco Noacco <francesco.noacco2000@gmail.com>
2025-04-03 16:18:13 +0200

    fix(hyprlock): remove custom prompt
    was introduced _somewhere_ and it's not needed anymore

--- home/modules/windowManagers/niri.nix
+++ home/modules/windowManagers/niri.nix
@@ -107,22 +107,6 @@ in
             blur_size = 5;
           }
         ];
-
-        input-field = [
-          {
-            size = "200, 50";
-            position = "0, -80";
-            monitor = "";
-            dots_center = true;
-            fade_on_empty = false;
-            font_color = "rgb(202, 211, 245)";
-            inner_color = "rgb(91, 96, 120)";
-            outer_color = "rgb(24, 25, 38)";
-            outline_thickness = 5;
-            placeholder_text = "<i>Input Password...</i>";
-            shadow_passes = 2;
-          }
-        ];
       };
     };
 
