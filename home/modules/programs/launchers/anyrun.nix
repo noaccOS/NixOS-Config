b7ff48bdd515bfaeb4a40bc6bf82d891bd97aed0
Francesco Noacco <francesco.noacco2000@gmail.com>
2025-11-14 08:35:56 +0100

    feat: enable anyrun daemon
    anyrun has become painfully slow on mayoi, using daemon mode fixes it

--- home/modules/programs/launchers/anyrun.nix
+++ home/modules/programs/launchers/anyrun.nix
@@ -6,30 +6,67 @@
   ...
 }:
 let
-  inherit (lib) mkEnableOption;
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    mkOption
+    getExe
+    types
+    ;
   cfg = config.homeModules.programs.launchers.anyrun;
 
 in
 {
   options.homeModules.programs.launchers.anyrun = {
     enable = mkEnableOption "anyrun";
-  };
 
-  config.programs.anyrun = {
-    enable = cfg.enable;
+    daemon = mkEnableOption "running anyrun as daemon" // {
+      default = true;
+    };
 
-    config = {
-      plugins = with inputs.anyrun.packages.${system}; [
-        applications
-        kidex
-        randr
-        rink
-        shell
-        stdin
-        symbols
-        websearch
-      ];
+    package = mkOption {
+      type = types.package;
+      default = inputs.anyrun.packages.${system}.default;
     };
   };
 
+  config = mkIf cfg.enable {
+    programs.anyrun = {
+      enable = true;
+
+      package = cfg.package;
+
+      config = {
+        plugins = with inputs.anyrun.packages.${system}; [
+          applications
+          kidex
+          randr
+          rink
+          shell
+          stdin
+          symbols
+          websearch
+        ];
+      };
+    };
+
+    systemd.user.services.anyrun = mkIf cfg.daemon {
+      Unit = {
+        Description = "Anyrun daemon";
+        PartOf = "graphical-session.target";
+        After = "graphical-session.target";
+      };
+
+      Service = {
+        Type = "simple";
+        ExecStart = "${getExe cfg.package} daemon";
+        Restart = "on-failure";
+        KillMode = "process";
+      };
+
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
+    };
+  };
 }
