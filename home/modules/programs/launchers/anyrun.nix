5329cdf9ee66408b1dd4cb917a0117a772546cf7
Francesco Noacco <francesco.noacco2000@gmail.com>
2025-11-14 09:34:11 +0100

    Revert "feat: enable anyrun daemon"
    This reverts commit b7ff48bdd515bfaeb4a40bc6bf82d891bd97aed0.

--- home/modules/programs/launchers/anyrun.nix
+++ home/modules/programs/launchers/anyrun.nix
@@ -6,67 +6,30 @@
   ...
 }:
 let
-  inherit (lib)
-    mkEnableOption
-    mkIf
-    mkOption
-    getExe
-    types
-    ;
+  inherit (lib) mkEnableOption;
   cfg = config.homeModules.programs.launchers.anyrun;
 
 in
 {
   options.homeModules.programs.launchers.anyrun = {
     enable = mkEnableOption "anyrun";
+  };
 
-    daemon = mkEnableOption "running anyrun as daemon" // {
-      default = true;
-    };
+  config.programs.anyrun = {
+    enable = cfg.enable;
 
-    package = mkOption {
-      type = types.package;
-      default = inputs.anyrun.packages.${system}.default;
+    config = {
+      plugins = with inputs.anyrun.packages.${system}; [
+        applications
+        kidex
+        randr
+        rink
+        shell
+        stdin
+        symbols
+        websearch
+      ];
     };
   };
 
-  config = mkIf cfg.enable {
-    programs.anyrun = {
-      enable = true;
-
-      package = cfg.package;
-
-      config = {
-        plugins = with inputs.anyrun.packages.${system}; [
-          applications
-          kidex
-          randr
-          rink
-          shell
-          stdin
-          symbols
-          websearch
-        ];
-      };
-    };
-
-    systemd.user.services.anyrun = mkIf cfg.daemon {
-      Unit = {
-        Description = "Anyrun daemon";
-        PartOf = "graphical-session.target";
-        After = "graphical-session.target";
-      };
-
-      Service = {
-        Type = "simple";
-        ExecStart = "${getExe cfg.package} daemon";
-        Restart = "on-failure";
-        KillMode = "process";
-      };
-
-      Install = {
-        WantedBy = [ "graphical-session.target" ];
-      };
-    };
-  };
 }
