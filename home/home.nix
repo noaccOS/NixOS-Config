f60ad12f937aef3109fb57352dec288e7599da71
Francesco Noacco <francesco.noacco2000@gmail.com>
2025-12-28 11:51:12 +0100

    chore: add nosevka cachix substituter

--- home/home.nix
+++ home/home.nix
@@ -19,9 +19,11 @@ in
     ];
     extra-substituters = [
       "https://nix-community.cachix.org"
+      "https://nosevka.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nosevka.cachix.org-1:vYPyn+9Z3qSZTKVagTtJ/4G8Mm4hivLYJo/APLVvUh8="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
