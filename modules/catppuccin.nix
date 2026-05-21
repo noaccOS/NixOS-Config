cf3564d79633d85599d12cb2de37394107e66fb6
Francesco Noacco <francesco.noacco2000@gmail.com>
2026-05-21 09:23:58 +0200

    fix: avoid compiling whiskers
    use nixpkgs version

--- modules/catppuccin.nix
+++ modules/catppuccin.nix
@@ -1,6 +1,19 @@
-{ config, user, ... }:
+{
+  config,
+  currentSystem,
+  inputs,
+  pkgs,
+  user,
+  ...
+}:
 {
   config.catppuccin = {
     inherit (config.home-manager.users.${user}.catppuccin) enable flavor accent;
+    sources = inputs.catppuccin.packages.${currentSystem}.overrideScope (
+      final: prev: {
+        whiskers = pkgs.catppuccin-whiskers;
+      }
+    );
+
   };
 }
