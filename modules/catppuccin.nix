efa70744412cb10757d21d422df493c7265cf218
Francesco Noacco <francesco.noacco2000@gmail.com>
2026-05-28 21:20:01 +0200

    refactor(catppuccin): use upstream cache

--- modules/catppuccin.nix
+++ modules/catppuccin.nix
@@ -1,19 +1,11 @@
 {
   config,
-  currentSystem,
-  inputs,
-  pkgs,
   user,
   ...
 }:
 {
   config.catppuccin = {
     inherit (config.home-manager.users.${user}.catppuccin) enable flavor accent;
-    sources = inputs.catppuccin.packages.${currentSystem}.overrideScope (
-      final: prev: {
-        whiskers = pkgs.catppuccin-whiskers;
-      }
-    );
-
+    cache.enable = true;
   };
 }
